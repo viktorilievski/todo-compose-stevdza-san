d7d2e91159fa97214eae1d0000355482bcf2c6f0
Viktor Ilievski <36957657+viktorilievski@users.noreply.github.com>
2022-07-25 12:11:16 +0200

    Initial setup navigation components, list and task screen

--- app/src/main/java/com/stevdzasan/courses/todocompose/MainActivity.kt
+++ app/src/main/java/com/stevdzasan/courses/todocompose/MainActivity.kt
@@ -3,23 +3,23 @@ package com.stevdzasan.courses.todocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.stevdzasan.courses.todocompose.navigation.SetupNavigation
 import com.stevdzasan.courses.todocompose.ui.theme.ToDoComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    
+    private lateinit var navController: NavHostController
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ToDoComposeTheme {
-
+                navController = rememberNavController()
+                SetupNavigation(navController = navController)
             }
         }
     }
---
+++ app/src/main/java/com/stevdzasan/courses/todocompose/navigation/destinations/TaskComposable.kt
@@ -0,0 +1,21 @@
+package com.stevdzasan.courses.todocompose.navigation.destinations
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.stevdzasan.courses.todocompose.util.Action
+import com.stevdzasan.courses.todocompose.util.Constants
+
+fun NavGraphBuilder.taskComposable(
+    navigateToListScreen: (Action) -> Unit
+) {
+    composable(
+        route = Constants.TASK_SCREEN,
+        arguments = listOf(navArgument(Constants.TASK_ARGUMENT_KEY) {
+            type = NavType.IntType
+        })
+    ) {
+
+    }
+}
