b7a24a06dbd2b9e64e75c2bfd376b83fbc51893b
Viktor Ilievski <36957657+viktorilievski@users.noreply.github.com>
2022-08-03 11:00:02 +0200

    Fix race condition bug with task screen

--- app/src/main/java/com/stevdzasan/courses/todocompose/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/stevdzasan/courses/todocompose/navigation/destinations/TaskComposable.kt
@@ -28,7 +28,7 @@ fun NavGraphBuilder.taskComposable(
         val selectedTask by sharedViewModel.selectedTask.collectAsState()
 
         LaunchedEffect(
-            key1 = taskId
+            key1 = selectedTask
         ) {
             sharedViewModel.updateTaskFields(selectedTask)
         }
