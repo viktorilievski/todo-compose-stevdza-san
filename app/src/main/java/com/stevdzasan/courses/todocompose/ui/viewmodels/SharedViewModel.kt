337561e27ac3df7f846fd3ab21a8745995dafc19
Viktor Ilievski <36957657+viktorilievski@users.noreply.github.com>
2022-08-05 15:51:08 +0200

    Create DataStore Repository

--- app/src/main/java/com/stevdzasan/courses/todocompose/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/stevdzasan/courses/todocompose/ui/viewmodels/SharedViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.stevdzasan.courses.todocompose.data.models.Priority
 import com.stevdzasan.courses.todocompose.data.models.ToDoTask
+import com.stevdzasan.courses.todocompose.data.repositories.DataStoreRepository
 import com.stevdzasan.courses.todocompose.data.repositories.ToDoRepository
 import com.stevdzasan.courses.todocompose.util.Action
 import com.stevdzasan.courses.todocompose.util.Constants.MAX_TITLE_LENGTH
@@ -22,7 +23,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SharedViewModel @Inject constructor(
-    private val repository: ToDoRepository
+    private val repository: ToDoRepository,
+    private val dataStoreRepository: DataStoreRepository
 ) : ViewModel() {
 
     val action: MutableState<Action> = mutableStateOf(Action.NO_ACTION)
