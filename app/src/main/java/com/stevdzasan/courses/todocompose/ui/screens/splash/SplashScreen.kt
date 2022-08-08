13820a6fc96a0c9dde4aa3856283625a1a088b89
Viktor Ilievski <36957657+viktorilievski@users.noreply.github.com>
2022-08-08 15:39:42 +0200

    Implement Splash Screen

--- app/src/main/java/com/stevdzasan/courses/todocompose/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/stevdzasan/courses/todocompose/ui/screens/splash/SplashScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -15,11 +16,18 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.stevdzasan.courses.todocompose.R
 import com.stevdzasan.courses.todocompose.ui.theme.LOGO_HEIGHT
-import com.stevdzasan.courses.todocompose.ui.theme.ToDoComposeTheme
 import com.stevdzasan.courses.todocompose.ui.theme.splashScreenBackground
+import com.stevdzasan.courses.todocompose.util.Constants.SPLASH_SCREEN_DELAY
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen() {
+fun SplashScreen(
+    navigateToListScreen: () -> Unit
+) {
+    LaunchedEffect(key1 = true) {
+        delay(SPLASH_SCREEN_DELAY)
+        navigateToListScreen()
+    }
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -47,5 +55,7 @@ fun getLogo(): Int {
 @Composable
 @Preview
 fun SplashScreenPreview() {
-    SplashScreen()
+    SplashScreen(
+        navigateToListScreen = {}
+    )
 }
